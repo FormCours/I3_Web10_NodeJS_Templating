015433552f8c9bb18b07141e5270c30933900322
Aurélien <aurelien.strimelle@bstorm.be>
2022-01-11 17:34:24 +0100

    Add view engin EJS

--- server.js
+++ server.js
@@ -1,19 +1,54 @@
 const http = require('http');
+const path = require('path');
+const ejs = require('ejs');
+
+// Méthode pour utiliser EJS
+const renderView = (res, viewName, data = {}) => {
+    // Méthode qui permet de générer le nom du fichier avec les séparateur adaptés
+    // => Evite les conflits entre les chemins windows et unix.
+    const fileName = path.resolve(__dirname, 'views', 'pages', viewName + '.ejs');
+    console.log(fileName);
+
+    // Utilisation du moteur de template EJS, pour convertir le fichier EJS
+    // en chaine de charactere contenant de l'html
+    ejs.renderFile(fileName, data, (error, contentHtml) => {
+
+        res.writeHead(200);
+        res.write(contentHtml);
+        res.end();
+    });
+};
 
 // Création d'un objet avec les routes du server
 const router = {
-    "/" : (req, res) => {
+    "/": (req, res) => {
+        const now = new Date();
+        const optionsDate = { year: 'numeric', month: 'long', day: 'numeric' };
 
-        res.writeHead(200);
-        res.write('<h1>Home</h1>');
-        res.end();
+
+        const dataHome = {
+            dateDuJour: now.toLocaleDateString('fr-be', optionsDate),
+            isDay: now.getHours() < 18 && now.getHours() > 8
+        };
+        renderView(res, 'home', dataHome);
     },
 
-    "/about" : (req, res) => {
+    "/about": (req, res) => {
 
-        res.writeHead(200);
-        res.write('<h1>About</h1>');
-        res.end();
+        renderView(res, 'about');
+    },
+
+    "/products": (req, res) => {
+        // Récuperation d'une liste de donnée (En reel -> Une base de donnée ou une API)
+        const category = "Food";
+        const products = [
+            { name: 'Pomme', price: 3.14 },
+            { name: 'Chocolat', price: 5 },
+            { name: 'Cerise', price: 2 },
+            { name: 'Fraise', price: 1 }
+        ];
+
+        renderView(res, 'products', { category, products });
     }
 };
 
@@ -22,7 +57,7 @@ const server = http.createServer((req, res) => {
     console.log(`Requete : [${req.method}] ${req.url}`);
 
     // Systeme de routing (Version simple)
-    if(req.url in router) {
+    if (req.url in router) {
         const action = router[req.url];
         action(req, res);
         return;
